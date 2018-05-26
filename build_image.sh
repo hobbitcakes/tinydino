f89b9ae1b9067ea28e04fee5535a9882e5932ab2
Jared <hobbitcakes@gmail.com>
2018-05-26 02:04:17 +0000

    proper docker hub user

--- build_image.sh
+++ build_image.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 docker stop tinydinopy && docker rm tinydinopy
-docker build -t petersonjared/tinydinopy:latest .
-docker run -d -p 5000:5000 --name tinydinopy petersonjared/tinydinopy:latest
+docker build -t hobbitcakes/tinydinopy:latest .
+docker run -d -p 5000:5000 --name tinydinopy hobbitcakes/tinydinopy:latest
 
 echo "1" ; sleep 1 
 echo "2" ; sleep 1 
